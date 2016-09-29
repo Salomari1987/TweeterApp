64704c4b775e42fc2b38e43dd67914739e16c8f5
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-09-29 13:25:45 +0400

    Configure session and cookieParser

--- config/middleware.js
+++ config/middleware.js
@@ -1,11 +1,15 @@
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var morgan = require('morgan');
+var session = require('express-session');
+var cookieParser = require('cookie-parser')
 
 module.exports = function ( app, express ) {
-  app.use(bodyParser.urlencoded( {extended: true} ));
-  app.use(bodyParser.json());
-  //Enable cross-orgin access
-  app.use(morgan('dev'));
-  app.use(express.static('client'));
+	app.use(bodyParser.urlencoded( {extended: true} ));
+	app.use(bodyParser.json());
+	app.use(cookieParser('tweet much?'));
+	app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }))
+	//Enable cross-orgin access
+	app.use(morgan('dev'));
+	app.use(express.static('client'));
 };
